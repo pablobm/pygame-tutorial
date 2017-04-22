237a00a7c2bc0c5811730e6f7e94f736dce40ec1
Pablo Brasero <pablo@pablobm.com>
2017-04-22 23:08:10 +0100

    Live reload and Markdown index page

--- config.rb
+++ config.rb
@@ -5,6 +5,10 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+activate :livereload
+
+set :markdown_engine, :redcarpet
+
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
---
+++ source/index.html.markdown
@@ -0,0 +1,7 @@
+---
+title: Pygame tutorial
+---
+
+# Pygame tutorial
+
+This is the Markdown index page.
