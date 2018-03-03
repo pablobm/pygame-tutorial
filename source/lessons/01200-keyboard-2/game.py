de0a36840c943c82bfd1ab859ec1b3246564dd66
Pablo Brasero <pablo@pablobm.com>
2018-03-03 13:59:26 +0000

    Implement challenge

--- source/lessons/01200-keyboard-2/game.py
+++ source/lessons/01200-keyboard-2/game.py
@@ -1,11 +1,28 @@
+import random
 import pygame
 
 BLACK = [0, 0, 0]
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+NUM_STARS = 200
 
 pygame.init()
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+
+class Star:
+    pass
+
+star_list = []
+i = 0
+while i < NUM_STARS:
+    new_star = Star()
+    new_star.x = random.randint(0, 800)
+    new_star.y = random.randint(0, 600)
+    new_star.size = random.randint(1, 3)
+    new_star.speed = random.randint(1, 3)
+    new_star.brightness = random.randint(0, 255)
+    star_list.append(new_star)
+    i += 1
 
 class Sprite:
     pass
@@ -33,6 +50,20 @@ while True:
     if keys[pygame.K_UP]:
         sprite.y = sprite.y - 5
 
+    for star in star_list:
+        if star.y > 600:
+            star.y = 0
+            star.x = random.randint(0, 800)
+        else:
+            star.y = star.y + star.speed
+
     screen.fill(BLACK)
+
+    for star in star_list:
+        color = [star.brightness, star.brightness, star.brightness]
+        position = [star.x, star.y]
+        pygame.draw.circle(screen, color, position, star.size)
+
     screen.blit(sprite.image, [sprite.x, sprite.y])
+
     pygame.display.flip()
